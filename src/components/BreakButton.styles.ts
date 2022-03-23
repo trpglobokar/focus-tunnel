866556099447bf022e75bc389e7096f3a10e8669
trpglobokar <trp.globokar@gmail.com>
2022-03-22 23:01:22 -0700

    feat: styled BreakButton w/ hover styles

--- src/components/BreakButton.styles.ts
+++ src/components/BreakButton.styles.ts
@@ -1,12 +1,12 @@
-import * as CSS from 'csstype';
+import { CSSObject } from "@emotion/react";
 
-export const breakButtonStyles: CSS.Properties = {
-  fontSize: '16px',
-  padding: '6px',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  background: 'white',
-  /*':hover': {
-    background: '#efefef',
-  },*/  //TODO: switch to emotion so i can use hover pseudo props
+export const breakButtonStyles: CSSObject = {
+  fontSize: "16px",
+  padding: "6px",
+  borderRadius: "4px",
+  cursor: "pointer",
+  background: "rgb(255 255 255 / 100%)",
+  ":hover": {
+    background: "rgb(255 255 255 / 50%)",
+  },
 };
--- src/components/BreakButton.tsx
+++ src/components/BreakButton.tsx
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
 import React, { FC } from "react";
 
 import { startNewBreakCountdown } from "./utils/actions";
@@ -8,15 +10,18 @@ interface BreakButtonProps {
   readonly isBreakAllowed: boolean;
   readonly triggerUpdateSiteBlockStatus: () => void;
 }
-export const BreakButton: FC<BreakButtonProps> = ({ isBreakAllowed, triggerUpdateSiteBlockStatus }) => {
+export const BreakButton: FC<BreakButtonProps> = ({
+  isBreakAllowed,
+  triggerUpdateSiteBlockStatus,
+}) => {
   const triggerBreak = () => {
     startNewBreakCountdown();
     triggerUpdateSiteBlockStatus();
   };
 
   return isBreakAllowed ? (
-    <button style={breakButtonStyles} onClick={triggerBreak}>
-        Take a Break
+    <button css={breakButtonStyles} onClick={triggerBreak}>
+      Take a Break
     </button>
-    ) : null;
+  ) : null;
 };
