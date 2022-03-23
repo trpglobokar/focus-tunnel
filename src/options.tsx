a8bd3a487aca8768c849a852e623b6e013867b97
trpglobokar <trp.globokar@gmail.com>
2022-03-23 14:56:54 -0700

    feat: displayed blocked sites and blocked hours in options page

--- src/options.tsx
+++ src/options.tsx
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { BlockedSite } from "./components/utils/blockedSiteList";
 
 //TODO: allow users to customize blocked sites & times here
 
-/*const Options = () => {
-  const [color, setColor] = useState<string>("");
+const WEEK_DAYS = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+const formatFocusHours = (focusHours: number[][]) => {
+  const listForm = focusHours.map((day, index) => (
+    <li key={index}>
+      {WEEK_DAYS[index]}: {day.join(",")}
+    </li>
+  ));
+
+  return <ul>{listForm}</ul>;
+};
+
+const Options = () => {
+  const [blockedSites, setBlockedSites] = useState<BlockedSite[]>([]);
   const [status, setStatus] = useState<string>("");
-  const [like, setLike] = useState<boolean>(false);
 
   useEffect(() => {
-    // Restores select box and checkbox state using the preferences
-    // stored in chrome.storage.
-    chrome.storage.sync.get(
-      {
-        favoriteColor: "red",
-        likesColor: true,
-      },
-      (items) => {
-        setColor(items.favoriteColor);
-        setLike(items.likesColor);
-      }
-    );
+    chrome.storage.sync.get({ blockedSites }, ({ blockedSites }) => {
+      setBlockedSites(blockedSites);
+    });
   }, []);
 
   const saveOptions = () => {
+    const newBlockedSites: BlockedSite[] = [];
     // Saves options to chrome.storage.sync.
     chrome.storage.sync.set(
       {
-        favoriteColor: color,
-        likesColor: like,
+        blockedSites: newBlockedSites,
       },
       () => {
         // Update status to let user know options were saved.
@@ -40,32 +43,21 @@ import ReactDOM from "react-dom";
       }
     );
   };
+  const SaveButton = <button onClick={saveOptions}>Save</button>;
+
+  const BlockedSiteList = blockedSites.map((site) => (
+    <li key={site.siteName}>
+      {site.siteName}
+      <br />
+      {formatFocusHours(site.focusHours)}
+    </li>
+  ));
 
   return (
     <>
-      <div>
-        Favorite color: <select
-          value={color}
-          onChange={(event) => setColor(event.target.value)}
-        >
-          <option value="red">red</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="yellow">yellow</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={like}
-            onChange={(event) => setLike(event.target.checked)}
-          />
-          I like colors.
-        </label>
-      </div>
+      <div>Blocked Sites:</div>
+      {BlockedSiteList}
       <div>{status}</div>
-      <button onClick={saveOptions}>Save</button>
     </>
   );
 };
@@ -75,4 +67,4 @@ ReactDOM.render(
     <Options />
   </React.StrictMode>,
   document.getElementById("root")
-);*/
+);
